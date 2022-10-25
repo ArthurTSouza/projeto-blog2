10f5dff3631b56d4c2c030c7c5f390c739321ea9
Arthur <ds-etim-1@PC115.ACADEMICO>
2022-10-25 09:29:07 -0300

    ultra complicado

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,11 +1,25 @@
 //cria uma variável para manipular um aquivo XML
-xmlhttp= new XMLHttpRequest();
+xmlhttp = new XMLHttpRequest();
 //lê um determinado arquivo xml no diretório do projeto
 xmlhttp.open("GET","xml/conteudo.xml",false);
 //envia o arquivo aberto para a leitura
 xmlhttp.send();
 //envia uma resposta do tipo XML
-xmlDoc= xmlhttp.responseXML;
+xmlDoc = xmlhttp.responseXML;
 
 //cria um array a partir da quantidade de postagens
-x= xmlDoc.getElementsByTagName("postagem");
+x = xmlDoc.getElementsByTagName("postagem");
+
+
+
+function funcaoConteudo(){
+    for(p=0; p<=2; p++){
+    document.write(
+        "<tr>" +
+            "<td>" + x[p].getAttribute("codigo") + "</td>" +
+            "<td>B</td>" +
+            "<td>C</td>" +
+            "<td>D</td>" +
+        "</tr>");
+    }
+}
