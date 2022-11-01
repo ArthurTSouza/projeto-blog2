7fa7522bd62cede5e9e475ec0e299c7e1d00ffd3
Arthur <ds-etim-1@PC115.ACADEMICO>
2022-10-31 21:24:33 -0300

    Código maluquinho

--- js/funcoes.js
+++ js/funcoes.js
@@ -13,13 +13,31 @@ x = xmlDoc.getElementsByTagName("postagem");
 
 
 function funcaoConteudo(){
-    for(p=0; p<=2; p++){
+    for(p=x.length-1; p>=0; p--){
+    document.write(
+        "<tr>" +
+            "<td>" + x[p].getAttribute("codigo") + "</td>"+
+            "<td> <img src='imgs/" + x[p].getElementsByTagName("imagem")[0].childNodes[0].nodeValue + "' width='150'></td>" +  
+            "<td><a href='postagem.html?codigo_postagem=" + p + "'>" + x[p].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</a></td>" +
+            "<td>" + x[p].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,150) + "</td>" +
+        "</tr>");
+    }
+}
+
+function funcaoPostagem(){
+ 
+    url= new URL(window.location.href);
+
+    parametro= url.searchParams;
+
+    p= parametro.get("codigo_postagem");
+
     document.write(
         "<tr>" +
             "<td>" + x[p].getAttribute("codigo")+"</td>"+
             "<td> <img src='imgs/" + x[p].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"' width='150'></td>" +  
             "<td>" + x[p].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</td>" +
-            "<td>" + x[p].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,150) + "</td>" +
+            "<td>" + x[p].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "</td>" +
         "</tr>");
-    }
+
 }
